376ca4ba47a96469af47dd2cc5655761c1e27a0a
HITRUST\arickyu <arick>
2023-02-21 13:46:12 +0800

    test

---
+++ src/main/java/com/example/testJenkins/TestJenkinsApplication.java
@@ -0,0 +1,15 @@
+package com.example.testJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class TestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJenkinsApplication.class, args);
+		System.out.println("123");
+	}
+
+}
