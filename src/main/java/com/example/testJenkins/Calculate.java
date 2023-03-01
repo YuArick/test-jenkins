69d85c100d78f65414c39e985e84e940baafd44e
HITRUST\arickyu <arick>
2023-03-01 10:33:05 +0800

    test1

--- src/main/java/com/example/testJenkins/Calculate.java
+++ src/main/java/com/example/testJenkins/Calculate.java
@@ -24,7 +24,7 @@ public class Calculate {
 	@ResponseBody
 	public String method2(@RequestBody Map<String, Object> params) {
 		String ans = Integer.parseInt((String) params.get("a")) - Integer.parseInt((String) params.get("b")) + "";
-		return ans;
+		return ans+1;
 	}
 	@RequestMapping("/method3")
 	@ResponseBody
